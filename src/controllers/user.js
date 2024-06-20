51dc972502161dcaa16c6df2744d72d3ddc61eaa
KeyurChanchad <chanchadkeyur2001@gmail.com>
2024-06-20 21:40:56 +0530

    Get url route declare

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,10 +1,36 @@
 const { ResponseCodes } = require("../../constants");
 const User = require("../models/User");
 const { encrypt } = require("../services/encrypt_decrypt");
-const { addUserValidation } = require("../validations/user");
+const { addUserValidation, updateUserValidation } = require("../validations/user");
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user_id); // Assuming the user ID is stored in req.user by authMiddleware
+    if (!user) {
+      return res.status(ResponseCodes.NOT_FOUND).send({
+        code: ResponseCodes.NOT_FOUND,
+        success: false,
+        message: "User not found",
+      });
+    }
+    return res.status(ResponseCodes.OK).json({
+      code: ResponseCodes.OK,
+      success: true,
+      message: "User get successfully.",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(ResponseCodes.FORBIDDEN).json({
+      code: ResponseCodes.FORBIDDEN,
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
 
 const updateUser = async (req, res) => {
-  return addUserValidation(req.body)
+  return updateUserValidation(req.body)
     .then(async (data) => {
       const updates = Object.keys(data);
       console.log("req body keys ", updates);
@@ -19,11 +45,10 @@ const updateUser = async (req, res) => {
         }
         console.log("data ", data);
         updates.forEach((update) => {
-          if(update === 'password'){
+          if (update === "password") {
             const encryptedPassword = encrypt(data[update]);
             user[update] = encryptedPassword;
-          }
-          else user[update] = data[update];
+          } else user[update] = data[update];
         });
         await user.save();
         return res.status(ResponseCodes.OK).json({
@@ -33,25 +58,21 @@ const updateUser = async (req, res) => {
           data: user,
         });
       } catch (error) {
-        return res
-          .status(ResponseCodes.FORBIDDEN)
-          .json({
-            code: ResponseCodes.FORBIDDEN,
-            success: false,
-            message: "Something went wrong",
-            error,
-          });
+        return res.status(ResponseCodes.FORBIDDEN).json({
+          code: ResponseCodes.FORBIDDEN,
+          success: false,
+          message: "Something went wrong",
+          error,
+        });
       }
     })
     .catch((error) => {
-      return res
-        .status(ResponseCodes.INTERNAL_SERVER_ERROR)
-        .json({
-          code: ResponseCodes.INTERNAL_SERVER_ERROR,
-          message: "Schema validation error",
-          error: error,
-          success: false,
-        });
+      return res.status(ResponseCodes.INTERNAL_SERVER_ERROR).json({
+        code: ResponseCodes.INTERNAL_SERVER_ERROR,
+        message: "Schema validation error",
+        error: error,
+        success: false,
+      });
     });
 };
 
@@ -74,18 +95,17 @@ const deleteUser = async (req, res) => {
       data: user,
     });
   } catch (error) {
-    return res
-      .status(ResponseCodes.FORBIDDEN)
-      .json({
-        code: ResponseCodes.FORBIDDEN,
-        success: false,
-        message: "Something went wrong",
-        error,
-      });
+    return res.status(ResponseCodes.FORBIDDEN).json({
+      code: ResponseCodes.FORBIDDEN,
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
   }
 };
 
 module.exports = {
   updateUser,
   deleteUser,
+  getUser
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
-const { updateUser, deleteUser } = require('../controllers/user');
+const { updateUser, deleteUser, getUser } = require('../controllers/user');
+
+// Get user profile
+router.get('/', authMiddleware, getUser);
 
 // Edit user profile
 router.put('/', authMiddleware, updateUser);
